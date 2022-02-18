d4972c403a2e88c514a703c2022cad294e0c2287
Michael Tobis <tobi4120@mylaurier.ca>
2022-02-17 22:27:11 -0500

    user info created

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,11 +1,13 @@
 import Header from "./header";
 import UserInfo from "../menu/userInfo";
+import Conversations from "./conversations";
 
 export default function Menu() {
     return (
         <div>
             <Header />
             <UserInfo />
+            <Conversations />
         </div>
     );
 }
--- src/menu/userInfo.js
+++ src/menu/userInfo.js
@@ -1,4 +1,5 @@
 import userProfilePic from "../assets/profilePictures/man-selfie.jpeg";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function userInfo() {
     return (
@@ -6,6 +7,8 @@ export default function userInfo() {
             <img src={userProfilePic} alt="profile pic" />
             <div className="name">
                 <h2>Bill Bradford</h2>
+                <SettingsIcon />
+                <p>Lead UX/UI Designer</p>
             </div>
         </div>
     );
